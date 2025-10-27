897708871b8b06b7680f90e5ece619bfa05d158d
Gael <2530240@upv.edu.mx>
2025-10-27 09:49:29 -0600

    esto es un repositorio de las metodologias de la programacion

---
+++ src/understanding_variables.py
@@ -0,0 +1,31 @@
+message = "This is my first python variable"
+another_message = "I am really, really, really happy"
+message = "I love python"
+
+# print() -> use to show messages in terminal
+# print() -> se utiliza para mostrar mensajes en la terminal
+print(message)
+print(another_message)
+print(message, another_message, message)
+print(another_message, message)
+
+print(message)
+
+"""
+
+Los nombres de variables en Python deben nombrarse solo con: 
+
+    - letras, números y guión bajo (espacios)
+    - deben comenzar con una letra o con un guión bajo, pero NO con número: 
+       ejemplo correcto: message_1 ( :) )
+       ejemplo incorrecto: 1_message (x)
+    - no utilizar espacios para separar los nombres en la variables 
+    - no utilizar palabras reservadas de Python para nombrar variables o archivos
+    - debeen ser cortos, pero descriptivos
+    - ingles
+    - minusculas
+
+"""
+
+gael_message = "Hola, soy Gael y estoy aprendiendo python"
+print(gael_mesage)
