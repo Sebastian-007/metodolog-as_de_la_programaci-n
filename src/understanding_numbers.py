81fba447c1cb32155899fa58e0abae7cd2036afb
Gael <2530240@upv.edu.mx>
2025-10-31 10:31:53 -0600

    feat: understanding numbers

---
+++ src/understanding_numbers.py
@@ -0,0 +1,69 @@
+# Numbers
+
+"""
+   Integers - Enteros 
+
+   Son números sin punto decimal
+   -infty, ..., -2, -1, 0, 1, 2, ..., infty
+
+   Ejemplo:
+
+       # Tipado dinamico
+       age = 33
+
+   Los podemos sumar (+), restar(-), multiplicar(*)
+   y dividir (/)
+
+   Potencias (**2, **3, )
+
+   Modulo (dividendo%divisor) 
+
+"""
+
+number_1 = 39
+number_2 = 13
+
+suma = number_1+number_2
+difference = number_1-number_2
+multiplication = number_1*number_2
+division = number_1/number_2
+modulo = number_1%number_2
+power = number_1**2
+
+print("suma", suma)
+print("difference", difference)
+print("multiplication", multiplication)
+print("division", division)
+print("modulo", modulo)
+print("power", power)
+
+print("La suma es el tipo", type (suma))
+print("La difference es el tipo", type (difference))
+print("La multiplication es el tipo", type (multiplication))
+print("La division es el tipo", type (division))
+print("La modulo es el tipo", type (modulo))
+print("La power es el tipo", type (power))
+
+# Floats 
+
+"""
+   Floats - Reales
+   Son números con punto decimal van desde -infty infty
+
+   Ejemplo: 
+      
+       # Tipado dinámico
+       age = 25.5
+   
+   Los podemos sumar(+), restar(-), multiplicar(*) y dividir(/)
+
+
+"""
+
+print(0.1+0.1)
+print(0.2-0.2)
+print(2*0.1)
+print(2*0.2)
+
+print(0.2+0.1)
+print(3*0.1)
